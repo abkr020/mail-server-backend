910772ef231def5baea5b944f4a92763bb9e4b9f
abkr020 <abkr.020@gmail.com>
2026-02-22 01:22:00 +0530

    added teh path in teh res.cookes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -75,6 +75,7 @@ export const login = async (req, res) => {
 
       sameSite: "none",      // CSRF protection
       // sameSite: "strict",      // CSRF protection
+       path: "/",  
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
