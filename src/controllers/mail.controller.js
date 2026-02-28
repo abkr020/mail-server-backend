1dd1c685f07e36b0096f2a778096b4eb0924d296
abkr020 <abkr.020@gmail.com>
2026-02-28 23:20:44 +0530

    get the mails from the inbound mail schema

--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -1,4 +1,5 @@
 import { Mail } from "../models/Mail.model.js";
+const InboundMail = require("../models/inboundMail.schema");
 
 export const getMyMails = async (req, res) => {
   try {
@@ -7,6 +8,26 @@ export const getMyMails = async (req, res) => {
 
 
     const mails = await Mail.find({ to: userEmail })
+      .sort({ createdAt: -1 })
+      .limit(50);
+
+    res.json({
+      count: mails.length,
+      mails,
+    });
+  } catch (error) {
+    console.error("Get mails error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getMyMails_fromInboundMailSchema = async (req, res) => {
+  try {
+    const userEmail = req.user_from_cookies.email; // from verified JWT
+
+    const mails = await InboundMail.find({
+      to: userEmail,
+    })
       .sort({ createdAt: -1 })
       .limit(50);
 
--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 // import { getMyMails } from "../controllers/mail.controller";
 // import { protect } from "../middlewares/auth.middleware";
 import { protect } from "../middlewares/auth.middleware.js";
-import { getMyMails } from "../controllers/mail.controller.js";
+import { getMyMails, getMyMails_fromInboundMailSchema } from "../controllers/mail.controller.js";
 import { sendMail } from "../controllers/sendMail.controller.js";
 import { getSentMails } from "../controllers/getSentMails.controller.js";
 
@@ -11,7 +11,8 @@ import { getSentMails } from "../controllers/getSentMails.controller.js";
 
 const router = express.Router();
 
-router.get("/my", protect, getMyMails);
+// router.get("/my", protect, getMyMails);
+router.get("/my", protect, getMyMails_fromInboundMailSchema);
 router.post("/send", protect, sendMail);
 router.get("/sent", protect, getSentMails);
 
